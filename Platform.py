617715a744159c21c2db910af60f402df1623d58
arjbarber <arjbarber@gmail.com>
2025-05-15 08:12:42 -0400

    added a new platform image

--- Platform.py
+++ Platform.py
@@ -1,11 +1,15 @@
 import pygame
 import config
+import os
 pygame.init()
+
+PLATFORM = pygame.image.load(os.path.join('assets', 'platform.png'))
 
 class Platform:
     def __init__(self, x, y, width, height, platform_number):
         self.platform_number: int = platform_number
         self.rect: pygame.Rect = pygame.Rect(x, y, width, height)
+        self.surface: pygame.Surface = pygame.transform.scale(PLATFORM, (self.rect.width, self.rect.height))
 
     def draw(self, surface):
-        pygame.draw.rect(surface, config.BLACK, self.rect)
+        surface.blit(self.surface, (self.rect.x, self.rect.y))
